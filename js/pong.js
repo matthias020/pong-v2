1206da79ad53f75c9822b91d385aeed755ba539f
Matthias <matthias.erselina@coderclass.nl>
2022-11-02 18:20:38 +0100

    Initial commit

---
+++ js/pong.js
@@ -0,0 +1,27 @@
+function sleep(ms) {
+    const date = Date.now();
+    let currentDate = null;
+    do {
+      currentDate = Date.now();
+    } while (currentDate - date < ms);
+  }
+
+function getViewportSize() {
+    let viewportSize = {
+        viewportWidth: $(window).width(),
+        viewportHeight: $(window).height()
+    };
+    
+    return viewportSize;
+}
+
+
+function setup() {
+
+}
+
+function draw() {
+    viewportSize = getViewportSize();
+    console.log(viewportSize);
+    sleep(1000);
+}
