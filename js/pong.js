e3048c58f395a39aef7184110bfc616003e668a9
Matthias <matthias.erselina@coderclass.nl>
2022-11-03 10:00:20 +0100

    Started with draw button function for menu of the game

--- js/pong.js
+++ js/pong.js
@@ -1,19 +1,40 @@
 function sleep(ms) {
-    const date = Date.now();
-    let currentDate = null;
-    do {
-      currentDate = Date.now();
-    } while (currentDate - date < ms);
-  }
+  const date = Date.now();
+  let currentDate = null;
+
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < ms);
+}
 
 function getViewportSize() {
-    let viewportSize = {
-        viewportWidth: $(window).width(),
-        viewportHeight: $(window).height()
-    };
-    
-    return viewportSize;
+  let viewportSize = {
+    width: $(window).width(),
+    height: $(window).height()
+  };
+
+  return viewportSize;
 }
+
+function getCenterBlockSize(viewportSize) {
+  let centerBlockSize = {
+    width: viewportSize.width / 350,
+    height: viewportSize.height / 60
+  };
+
+  return centerBlockSize;
+}
+function drawCenterLine(centerBlockSize, viewportSize) {
+  let centerLineX = viewportSize.width / 2 - centerBlockSize.width / 2;
+  let currentBlockY = centerBlockSize.height / 2;
+
+  while (currentBlockY < viewportSize.height) {
+    rect(centerLineX, currentBlockY, centerBlockSize.width, centerBlockSize.height);
+    currentBlockY = currentBlockY + centerBlockSize.height * 2;
+  }
+}
+
+function drawButton(buttonX, buttonY, buttonWidth, buttonHeight, textSize, text)
 
 
 function setup() {
@@ -21,7 +42,18 @@ function setup() {
 }
 
 function draw() {
-    viewportSize = getViewportSize();
-    console.log(viewportSize);
-    sleep(1000);
+  clear();
+
+  let viewportSize = getViewportSize();
+  createCanvas(viewportSize.width, viewportSize.height);
+  background(0);
+
+  let shapeColor = color(255);
+  fill(shapeColor);
+  noStroke();
+
+  let centerBlockSize = getCenterBlockSize(viewportSize);
+  drawCenterLine(centerBlockSize, viewportSize);
+
+  
 }
