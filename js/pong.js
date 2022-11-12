94a836a1e13f867b384e306726c530ae389625fc
Matthias <matthias.erselina@coderclass.nl>
2022-11-12 16:20:05 +0100

    Still cleaning code

--- js/pong.js
+++ js/pong.js
@@ -33,6 +33,7 @@ function setup() {
   globalVars = {
     goTo: "initCenterLine",
     transitionFrame: 0,
+    duringTransition: false, //
 
     logo: {
       overlayOpacity: 0,
@@ -74,27 +75,32 @@ function getViewportSize() {
   return viewportSize;
 }
 
-function getCenterBlockSize(viewportSize) {
-  let centerBlockSize = {
+//function getcenterLineBlockSize(viewportSize) {
+//  let centerLineBlockSize = {
+//    width: viewportSize.width / 350,
+//    height: viewportSize.height / 60
+//  };
+
+//  return centerLineBlockSize;
+//}
+function drawCenterLine(viewportSize) {
+  let centerLineBlockSize = {
     width: viewportSize.width / 350,
     height: viewportSize.height / 60
   };
 
-  return centerBlockSize;
-}
-function drawCenterLine(centerBlockSize, viewportSize, duringTransition) {
-  if (duringTransition == "fadeIn") {
-    let currentOpacity = globalVars.transitionFrame * conf.opacityStep;
-    fill(255, 255, 255, currentOpacity);
-    console.log("centerLine: " + currentOpacity);
+  if (globalVars.duringTransition == "fadeIn") {
+    let centerLineCurrentOpacity = globalVars.transitionFrame * conf.opacityStep;
+    fill(255, 255, 255, centerLineCurrentOpacity);
+    console.log("centerLine: " + centerLineCurrentOpacity);
   }
 
-  let centerLineX = viewportSize.width / 2 - centerBlockSize.width / 2;
-  let currentBlockY = centerBlockSize.height / 2;
+  let centerLineX = viewportSize.width / 2 - centerLineBlockSize.width / 2;
+  let centerLineBlockCurrentY = centerLineBlockSize.height / 2;
 
-  while (currentBlockY < viewportSize.height) {
-    rect(centerLineX, currentBlockY, centerBlockSize.width, centerBlockSize.height);
-    currentBlockY = currentBlockY + centerBlockSize.height * 2;
+  while (centerLineBlockCurrentY < viewportSize.height) {
+    rect(centerLineX, centerLineBlockCurrentY, centerLineBlockSize.width, centerLineBlockSize.height);
+    centerLineBlockCurrentY = centerLineBlockCurrentY + centerLineBlockSize.height * 2;
   }
 }
 
@@ -104,23 +110,25 @@ function showLogo(logoImage,
                   logoSize, 
                   logoMiddleX, 
                   logoMiddleY,
-                  logoURL, 
-                  duringTransition) {
+                  logoURL) {
   
   tint(255, 255);
-  if (duringTransition == "fadeIn") {
-    let currentOpacity = globalVars.transitionFrame * conf.opacityStep;
-    tint(255, currentOpacity);
-    console.log("Logo: " + currentOpacity);
-  } else if (duringTransition == "fadeOut") {
-    let currentOpacity = 255 - globalVars.transitionFrame * conf.opacityStep;
-    tint(255, currentOpacity);
-    console.log("Logo: " + currentOpacity);
+
+  if (globalVars.duringTransition != false) {
+    let logoOverlayCurrentTransparency = globalVars.transitionFrame * conf.opacityStep;
+
+    if (globalVars.duringTransition == "fadeOut") {
+      logoOverlayCurrentTransparency = 255 - logoOverlayCurrentTransparency;
+    }
+
+    tint(255, logoOverlayCurrentTransparency);
+    console.log("Logo: " + logoOverlayCurrentTransparency);
   }
 
   image(logoImage, logoX, logoY, logoSize, logoSize);
   
-  if (dist(mouseX, mouseY, logoMiddleX, logoMiddleY) < logoSize / 2 && duringTransition == false) {
+  mouseDistanceLogoMiddle = dist(mouseX, mouseY, logoMiddleX, logoMiddleY);
+  if (mouseDistanceLogoMiddle < logoSize / 2 && globalVars.duringTransition == false) {
     if (mouseIsPressed == true 
         && mouseButton == LEFT 
         && globalVars.logo.clickDisableCount > 15) {
@@ -130,11 +138,11 @@ function showLogo(logoImage,
       mouseIsPressed = false;
     }
 
-    if (globalVars.logo.overlayOpacity < 80 && duringTransition == false) {
+    if (globalVars.logo.overlayOpacity < 80 && globalVars.duringTransition == false) {
       globalVars.logo.overlayOpacity += 5;
     }
   } else {
-    if (globalVars.logo.overlayOpacity > 0 && duringTransition == false) {
+    if (globalVars.logo.overlayOpacity > 0 && globalVars.duringTransition == false) {
       globalVars.logo.overlayOpacity -= 5;
     }
   }
@@ -150,12 +158,11 @@ function drawButton(buttonX,
                     buttonColorOriginal, 
                     buttonColorHover, 
                     buttonCornerRadius, 
-                    textColorOriginal, 
-                    textColorHover, 
-                    textSizeOwn, 
-                    textFontOwn, 
-                    textOwn, 
-                    duringTransition, 
+                    buttonTextColorOriginal, 
+                    buttonTextColorHover, 
+                    buttonTextSize, 
+                    buttonTextFont, 
+                    buttonText, 
                     changeGoToTo) {
 
   let buttonTransitionPerFrame = {
@@ -163,10 +170,10 @@ function drawButton(buttonX,
     g: (buttonColorHover.g - buttonColorOriginal.g) / conf.amountFramesTransition,
     b: (buttonColorHover.b - buttonColorOriginal.b) / conf.amountFramesTransition
   };
-  let textTransitionPerFrame = {
-    r: (textColorHover.r - textColorOriginal.r) / conf.amountFramesTransition,
-    g: (textColorHover.g - textColorOriginal.g) / conf.amountFramesTransition,
-    b: (textColorHover.b - textColorOriginal.b) / conf.amountFramesTransition
+  let buttonTextTransitionPerFrame = {
+    r: (buttonTextColorHover.r - buttonTextColorOriginal.r) / conf.amountFramesTransition,
+    g: (buttonTextColorHover.g - buttonTextColorOriginal.g) / conf.amountFramesTransition,
+    b: (buttonTextColorHover.b - buttonTextColorOriginal.b) / conf.amountFramesTransition
   };
 
   if (mouseX > buttonX 
@@ -179,22 +186,22 @@ function drawButton(buttonX,
       mouseIsPressed = false;
     }
     
-    if (globalVars.button.transitionFrame < conf.amountFramesTransition && duringTransition == false) {
+    if (globalVars.button.transitionFrame < conf.amountFramesTransition && globalVars.duringTransition == false) {
       globalVars.button.transitionFrame++;
     }
   } else {
-    if (globalVars.button.transitionFrame > 0 && duringTransition == false) {
+    if (globalVars.button.transitionFrame > 0 && globalVars.duringTransition == false) {
       globalVars.button.transitionFrame--;
     }
   }
 
-  let currentOpacity = 255;
-  if (duringTransition == "fadeIn") {
-    currentOpacity = globalVars.transitionFrame * conf.opacityStep;
-    console.log("button: " + currentOpacity);
-  } else if (duringTransition == "fadeOut") {
-    currentOpacity = 255 - globalVars.transitionFrame * conf.opacityStep;
-    console.log("button: " + currentOpacity);
+  let buttonCurrentOpacity = 255;
+  if (globalVars.duringTransition == "fadeIn") {
+    buttonCurrentOpacity = globalVars.transitionFrame * conf.opacityStep;
+    console.log("button: " + buttonCurrentOpacity);
+  } else if (globalVars.duringTransition == "fadeOut") {
+    buttonCurrentOpacity = 255 - globalVars.transitionFrame * conf.opacityStep;
+    console.log("button: " + buttonCurrentOpacity);
   }
 
   let buttonColor = {
@@ -202,31 +209,31 @@ function drawButton(buttonX,
     g: buttonColorOriginal.g + buttonTransitionPerFrame.g * globalVars.button.transitionFrame,
     b: buttonColorOriginal.b + buttonTransitionPerFrame.b * globalVars.button.transitionFrame
   };
-  let buttonColorReady = color(buttonColor.r, buttonColor.g, buttonColor.b, currentOpacity);
-  let textColor = {
-    r: textColorOriginal.r + textTransitionPerFrame.r * globalVars.button.transitionFrame,
-    g: textColorOriginal.g + textTransitionPerFrame.g * globalVars.button.transitionFrame,
-    b: textColorOriginal.b + textTransitionPerFrame.b * globalVars.button.transitionFrame
+  let buttonColorReady = color(buttonColor.r, buttonColor.g, buttonColor.b, buttonCurrentOpacity);
+  let buttonTextColor = {
+    r: buttonTextColorOriginal.r + buttonTextTransitionPerFrame.r * globalVars.button.transitionFrame,
+    g: buttonTextColorOriginal.g + buttonTextTransitionPerFrame.g * globalVars.button.transitionFrame,
+    b: buttonTextColorOriginal.b + buttonTextTransitionPerFrame.b * globalVars.button.transitionFrame
   };
-  let textColorReady = color(textColor.r, textColor.g, textColor.b, currentOpacity);
+  let buttonTextColorReady = color(buttonTextColor.r, buttonTextColor.g, buttonTextColor.b, buttonCurrentOpacity);
 
   fill(buttonColorReady);
   rect(buttonX, buttonY, buttonWidth, buttonHeight, buttonCornerRadius);
-  fill(textColorReady);
-  textFont(textFontOwn);
-  textSize(textSizeOwn);
+  fill(buttonTextColorReady);
+  textFont(buttonTextFont);
+  textSize(buttonTextSize);
   textAlign(CENTER, CENTER);
-  text(textOwn, buttonX, buttonY, buttonWidth, buttonHeight);
+  text(buttonText, buttonX, buttonY, buttonWidth, buttonHeight);
 }
 
-function start(viewportSize, duringTransition) {
+function start(viewportSize) {
   let logoSize = viewportSize.totalSqrt / 5.5;
   let logoX = viewportSize.width / 2 - logoSize / 2;
   let logoY = viewportSize.height / 3 - logoSize / 2;
   let logoMiddleX = viewportSize.width / 2;
   let logoMiddleY = viewportSize.height / 3;
   globalVars.logo.clickDisableCount++;
-  showLogo(conf.logo.image, logoX, logoY, logoSize, logoMiddleX, logoMiddleY, conf.logo.URL, duringTransition);
+  showLogo(conf.logo.image, logoX, logoY, logoSize, logoMiddleX, logoMiddleY, conf.logo.URL);
 
   let buttonWidth = viewportSize.width / 4;
   let buttonHeight = viewportSize.height / 6;
@@ -267,8 +274,7 @@ function start(viewportSize, duringTransition) {
             textColorHoverLoose, 
             textSizeOwn, 
             textFontOwn, 
-            textOwn, 
-            duringTransition,
+            textOwn,
             changeGoToTo);
 }
 
@@ -291,34 +297,34 @@ function draw() {
   //  fpsCount = 0;
   //}
 
-  let centerBlockSize = getCenterBlockSize(viewportSize);
+  //let centerLineBlockSize = getcenterLineBlockSize(viewportSize);
   if (globalVars.goTo != "initCenterLine") {
-    let duringTransition = false;
-    drawCenterLine(centerBlockSize, viewportSize, duringTransition);
+    globalVars.duringTransition = false;
+    drawCenterLine(viewportSize);
   }
 
   if (globalVars.goTo == "initCenterLine") {
-    let duringTransition = "fadeIn";
-    drawCenterLine(centerBlockSize, viewportSize, duringTransition);
+    globalVars.duringTransition = "fadeIn";
+    drawCenterLine(viewportSize);
     if (globalVars.transitionFrame == conf.amountFramesTransition) {
       globalVars.transitionFrame = 1;
       globalVars.goTo = "initStart";
     }
     globalVars.transitionFrame++;
   } else if (globalVars.goTo == "initStart") {
-    let duringTransition = "fadeIn";
-    start(viewportSize, duringTransition);
+    globalVars.duringTransition = "fadeIn";
+    start(viewportSize);
     if (globalVars.transitionFrame == conf.amountFramesTransition) {
       globalVars.transitionFrame = 1;
       globalVars.goTo = "start";
     }
     globalVars.transitionFrame++;
   } else if (globalVars.goTo == "start") {
-    let duringTransition = false;
-    start(viewportSize, duringTransition);
+    globalVars.duringTransition = false;
+    start(viewportSize);
   } else if (globalVars.goTo == "exitStart") {
-    let duringTransition = "fadeOut";
-    start(viewportSize, duringTransition);
+    globalVars.duringTransition = "fadeOut";
+    start(viewportSize);
     if (globalVars.transitionFrame == conf.amountFramesTransition) {
       globalVars.transitionFrame = 1;
       globalVars.goTo = "next";
